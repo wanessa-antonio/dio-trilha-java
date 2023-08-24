e8da124da858e147978208fc6ea5922db976fa82
Wanessa <wanessa.ant04@gmail.com>
2023-08-24 09:41:51 -0300

    atualizando conta-banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
-public class ContaTerminal {    
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public class ContaTerminal {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.print("Por favor, digite o número da Conta: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Limpar o buffer após a leitura do número inteiro
+
+        System.out.print("Por favor, digite o nome do Cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.print("Por favor, digite o saldo da Conta: ");
+        double saldo = scanner.nextDouble();
+
+        scanner.close();
+
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.";
+
+        System.out.println(mensagem);
     }
 }
